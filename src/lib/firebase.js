9df478171a149c691060694193bb8b85135996f1
Ollynov <ollynov@gmail.com>
2023-05-08 14:19:52 -0700

    added firebase hosting

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQZsokjEIM02ubM9T45VAInf9ZzZ8-wYk",
+  authDomain: "wordle-af6b0.firebaseapp.com",
+  projectId: "wordle-af6b0",
+  storageBucket: "wordle-af6b0.appspot.com",
+  messagingSenderId: "946592410942",
+  appId: "1:946592410942:web:8b1655c026f41076e6d06f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
