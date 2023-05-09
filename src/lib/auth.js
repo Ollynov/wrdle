bae339c2ab912980ec9f9a20489e0708bd45d030
Ollynov <ollynov@gmail.com>
2023-05-08 17:15:51 -0700

    adding initial code for saving user data

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,21 +1,40 @@
 // import Toastify from "toastify-js";
 // import "toastify-js/src/toastify.css";
-import { auth } from "./firebase";
+import { auth, firestore } from "./firebase";
 import {
   GoogleAuthProvider,
   signInWithPopup,
   browserPopupRedirectResolver,
 } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 
 const googleProvider = new GoogleAuthProvider();
 
 // const auth = getAuth();
+export const createUser = async (uid, data) => {
+  const docRef = doc(firestore, "users", uid);
+  return setDoc(docRef, data, { merge: true })
+    .then(() => {
+      return "successfully saved";
+    })
+    .catch((err) => {
+      console.log("error creating user: ", err);
+      throw err;
+    });
+};
 
 export async function googleLogin() {
   // return signInWithPopup(auth, googleProvider)
   return signInWithPopup(auth, googleProvider, browserPopupRedirectResolver)
     .then((res) => {
-      if (res.user) console.log("ok logged in user: ", res.user);
+      if (res.user.metadata.creationTime === res.user.metadata.lastSignInTime) {
+        // check in case someone clicked on 'sign in' instead of 'signup' the first time, since in that case we should create new user:
+        createUser(res.user.uid, {
+          uid: res.user.uid,
+          email: res.user.email,
+          name: res.user.displayName,
+        });
+      }
 
       return res.user;
     })
